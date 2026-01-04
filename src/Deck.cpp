f71a7c0fccee7d8a77a1c11bd89d9948603f45e9
Kkeenon0 <Kkeenon0@gmail.com>
2026-01-04 15:58:27 -0800

    Implement Deck class

---
+++ src/Deck.cpp
@@ -0,0 +1,29 @@
+#include "Deck.h"
+#include <algorithm>
+#include <random>
+
+using namespace std;
+
+Deck::Deck() : currentIndex(0) {
+  string suits[] = {"Hearts", "Diamonds", "Cloves", "Spades"};
+  string ranks[] = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+  int values[] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
+
+  for (const string& suit: suits){
+    for(int i = 0; i < 13; i++){
+      cards.emplace_back(ranks[i], suit, values[i]);
+    }
+  }
+}
+
+void Deck::shuffle() {
+  random_device rd;
+  mt19937 g(rd());
+  shuffle(cards.begin(), cards.end(), g);
+  currentIndex = 0;
+}
+
+Card Deck::dealCard(){
+  return cards[currentIndex++];
+}
+  
