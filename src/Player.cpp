eca776595ffd44a5e30feb1f5c4d071eb6624d7d
Kkeenon0 <Kkeenon0@gmail.com>
2026-01-05 12:00:45 -0800

    Implement Player class

---
+++ src/Player.cpp
@@ -0,0 +1,19 @@
+#include "Player.h"
+using namespace std;
+
+Player::Player(const string& n) : name(n) {}
+
+string Player::getName() const{
+    return name;
+}
+
+Hand& Player::getHand(){
+    return hand;
+}
+const Hand& Player::getHand() const{
+    return hand
+      }
+
+void Player::clearHand(){
+    hand.clear(); 
+}
